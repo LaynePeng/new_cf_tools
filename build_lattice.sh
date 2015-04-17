fe2242db972b7dbab0a1acea42dbb603b8f5af7a
LaynePeng <appamail@hotmail.com>
2015-04-18 03:54:21 +0800

    1. Update for latest Lattice build, which change the script path; 2. Fix the bug that sometime unable to clean the directory cp to Diego-Realse.
    Signed-off-by: LaynePeng <appamail@hotmail.com>

--- build_lattice.sh
+++ build_lattice.sh
@@ -5,15 +5,17 @@ set -e
 export DOCKER_WORKSPACE_MOUNT_DIR="/media/sf_Codes"
 export LATTICE_SRC_PATH="/workspace/lattice/lattice"
 export DIEGO_RELEASE_PATH="/workspace/Diego/diego-release"
+export DIEGO_LATTICE_LIB=$DIEGO_RELEASE_PATH"/src/github.com/cloudfoundry-incubator/lattice"
 export CF_RELEASE_PATH="/workspace/CF/cf-release"
 export DOCKER_IMAGE="cloudfoundry/lattice-pipeline"
 
-CMD="echo Start to compile. && pushd $DIEGO_RELEASE_PATH && ./scripts/update && popd && cp -rf $LATTICE_SRC_PATH $DIEGO_RELEASE_PATH/src/github.com/cloudfoundry-incubator &&$LATTICE_SRC_PATH/compile $LATTICE_SRC_PATH/lattice-build $DIEGO_RELEASE_PATH $CF_RELEASE_PATH $LATTICE_SRC_PATH && echo Creating lattice.tgz && cd $LATTICE_SRC_PATH && tar cvzf lattice.tgz lattice-build && echo Start to cleaning. && rm -rf $LATTICE_SRC_PATH/lattice-build && rm -rf $DIEGO_RELEASE_PATH/src/github.com/cloudfoundry-incubator/lattice"
+CMD="echo Start to compile. && pushd $DIEGO_RELEASE_PATH && ./scripts/update && popd && mkdir $DIEGO_LATTICE_LIB && pushd $LATTICE_SRC_PATH && tar -c --exclude .git . | tar -x -C $DIEGO_LATTICE_LIB && popd && $LATTICE_SRC_PATH/cluster/scripts/compile $LATTICE_SRC_PATH/lattice-build $DIEGO_RELEASE_PATH $CF_RELEASE_PATH $LATTICE_SRC_PATH && echo Creating lattice.tgz && cd $LATTICE_SRC_PATH && tar cvzf lattice.tgz lattice-build && echo Start to cleaning. && rm -rf $LATTICE_SRC_PATH/lattice-build && rm -rf $DIEGO_LATTICE_LIB"
 
 docker run --rm -a stdout -a stderr -w /workspace \
   -v ${DOCKER_WORKSPACE_MOUNT_DIR:-~/workspace/}:/workspace \
   -e LATTICE_SRC_PATH \
   -e DIEGO_RELEASE_PATH \
+  -e DIEGO_LATTICE_LIB \
   -e CF_RELEASE_PATH \
   -e DRY_RUN \
   -e LATTICE_RELEASE_VERSION \
